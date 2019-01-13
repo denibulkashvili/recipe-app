6b55dd096029d564abc4f866fbb55f97c9420f6d
denibulkashvili <denibulkashvili@gmail.com>
2019-01-13 10:49:48 +0700

    Update App.module.css

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './App.module.css'
 import { ThemeProvider } from 'styled-components';
 
 const theme = {
