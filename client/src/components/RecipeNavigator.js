304b0a7f8be056309ccd80c3dc255235298ff214
denibulkashvili <denibulkashvili@gmail.com>
2019-01-13 10:38:48 +0700

    Style Recipe Navigator and recipe viewer

--- client/src/components/RecipeNavigator.js
+++ client/src/components/RecipeNavigator.js
@@ -7,12 +7,23 @@ const RecipeList = ({ recipes, onClick }) => {
     }
 }
 
+const List = styled.ul`
+  width: 400px;
+  height: 95vh;
+  padding: 20px;
+  margin-top: 0;
+  background: ${props => props.theme.primaryLight};
+  color: ${props => props.theme.textColor};
+  flex-grow: 1;
+  overflow-y: scroll;
+`;
 const StyledRecipeListItem = styled.li`
   font-size: 20px;
   list-style: none;
   padding: 5px;
-  white-space: nowrap;
-`
+  border-bottom: 1px solid ${props => props.theme.textColor};
+  max-height: 100vh;
+`;
 
 const RecipeNavigator = (props) => {
     return (
@@ -22,10 +33,7 @@ const RecipeNavigator = (props) => {
     )
 }
 
-const List = styled.ul`
-  width: auto;
-  margin: 10px;
-`
+
 
 
 
